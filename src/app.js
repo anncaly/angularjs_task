66ecbdb3529dc5ad137e275ff9fe1ad67a307bc3
Ann Caly <anncaly@gmail.com>
2018-08-17 13:12:07 +0300

    change tabs to spaces

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 angular
-	.module("myApp", [])
-	.controller('parentCtrl', function() {
+  .module("myApp", [])
+  .controller('parentCtrl', function() {
     this.click = function(value) {
       console.log(value);
     }
   })
-	.directive('container', function() {
-	  return {
-	    restrict: 'E',
-	    transclude: true,
-	    controller: 'parentCtrl',
-	    controllerAs: 'parent',
-	    templateUrl: 'container.html',
-	  };
-	})
-	.directive('myButton', function () {
+  .directive('container', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      controller: 'parentCtrl',
+      controllerAs: 'parent',
+      templateUrl: 'container.html',
+    };
+  })
+  .directive('myButton', function () {
     return {
       restrict: 'E',
       transclude: true,
