15ba27f079e0c26c30aecb660cf619620080b0f4
Ann Caly <anncaly@gmail.com>
2018-08-17 13:02:57 +0300

    done

---
+++ src/app.js
@@ -0,0 +1,23 @@
+angular
+	.module("myApp", [])
+	.controller('parentCtrl', function() {
+    this.click = function(value) {
+      console.log(value);
+    }
+  })
+	.directive('container', function() {
+	  return {
+	    restrict: 'E',
+	    transclude: true,
+	    controller: 'parentCtrl',
+	    controllerAs: 'parent',
+	    templateUrl: 'container.html',
+	  };
+	})
+	.directive('myButton', function () {
+    return {
+      restrict: 'E',
+      transclude: true,
+      templateUrl: 'my-button.html',
+    };
+});
